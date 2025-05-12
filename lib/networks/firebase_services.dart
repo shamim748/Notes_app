deadc931c7ee145b1319d156319c1cef29594835
Md Shamim <nazrulislamshamim48@gmail.com>
2025-05-13 01:40:14 +0600

    network related things

--- lib/networks/firebase_services.dart
+++ lib/networks/firebase_services.dart
@@ -1,16 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:note_app/helpers/custom_snackbar.dart';
 
 class FirebaseServices {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future<void> createUser({
+    required BuildContext context,
     required String userId,
     required Map<String, dynamic> userData,
   }) async {
     try {
       await _firestore.collection('users').doc(userId).set(userData);
     } catch (e) {
-      CustomSnackbar().failedSnackBar(message: e.toString());
+      if (context.mounted) {
+        CustomSnackbar().failedSnackBar(
+          context: context,
+          message: e.toString(),
+        );
+      }
       ;
     }
   }
